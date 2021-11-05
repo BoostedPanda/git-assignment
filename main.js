95be457093283b4c59e9884074948f38d04ad328
Kevin Ujfalusi <kevinujfalusi@hotmail.se>
2021-11-05 10:58:09 +0100

    add done list to html, js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const todoInput = document.querySelector("#todoInput")
 const todoBtn = document.querySelector("#todoBtn")
 const todoList = document.querySelector("#todoList")
+const doneList = document.querySelector("#donelist")
 
 todoBtn.addEventListener("click", () => {
     const myLi = document.createElement("li")
